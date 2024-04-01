c94f99a5a4cdbe2cf5da2642450036ac4aa4ec8e
Kaustubh <kaustubhr2001@gmail.com>
2024-04-01 12:01:35 +0530

    cloudenary setup done

--- index.js
+++ index.js
@@ -3,14 +3,18 @@ const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
 const dbConnect = require('./Utils/dbconnect');
+const cors = require('cors');
+
+dbConnect();
+
 
 const PORT = process.env.PORT || 5000;
-dbConnect();
 
 
 
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cors());
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
