2a47d0ee15d9dbc2c77ab08158e20a3feb4d5352
Kaustubh <kaustubhr2001@gmail.com>
2024-04-05 21:18:43 +0530

    code fixes

--- Utils/dbconnect.js
+++ Utils/dbconnect.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const dbConnect = async() => {
     try {
+        console.log("URL: ", process.env.MONGO_URI);
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
--- index.js
+++ index.js
@@ -1,43 +1,29 @@
-const router = require("express").Router();
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./Utils/dbconnect');
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-const {
-    add_assignment,
-    get_assignments,
-    get_totalAssignment,
-    get_assignment_details,
-    refresh_get_assignment_details,
+// Connect Database
+connectDB();
 
-} = require("../Controllers/Assignment");
+// Init Middleware
+app.use(cors());
+app.use(express.json());
 
-const {
-    update_assignment_Details
-} = require("../Controllers/UpdateAssignment");
-
-router.post("/addassignment", add_assignment);
-router.get("/getassignments", get_assignments);
-router.get("/gettotalassignment", get_totalAssignment);
-router.post("/getassignmentdetails", get_assignment_details);
-router.post("/refreshgetassignmentdetails/:assignmentId", refresh_get_assignment_details);
-
-router.post("/updateassignmentdetails", update_assignment_Details);
-
-module.exports = router;
+// Define Routes
 app.use('/api/user', require('./Routes/user'));
 app.use('/api/employee', require('./Routes/employees'));
 app.use('/api/assignment', require('./Routes/assignment'));
 app.use('/api/auth', require('./Routes/auth'));
-/* The line `app.use('/api/terms', require('./Routes/terms'));` is setting up a route in the Express
-application. When a request is made to the '/api/terms' endpoint, it will be handled by the routes
-defined in the './Routes/terms' file. This allows for modularizing the routes in the application and
-organizing them based on their functionality. */
 app.use('/api/terms', require('./Routes/terms'));
 
-
+// Default route
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
-
+// Start server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
