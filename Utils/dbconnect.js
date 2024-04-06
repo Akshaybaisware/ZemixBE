5d43c36ceb2e56d5119ed5b04df8cbe4efdc5bca
Kaustubh <kaustubhr2001@gmail.com>
2024-04-06 22:21:38 +0530

    some config indb file removed

--- Utils/dbconnect.js
+++ Utils/dbconnect.js
@@ -5,10 +5,7 @@ dotenv.config();
 const dbConnect = async() => {
     try {
         console.log("URL: ", process.env.MONGO_URI);
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB connected');
     } catch (error) {
         console.log('MongoDB connection failed');
